9b61f658e6c34f44db02f82a4508dfa1ff759ca0
sonmu <sonmu.def@gmail.com>
2025-03-02 01:12:34 +0800

    fix unocss-useful lead lsp not working

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,16 @@
 // unocss.config.ts
 import { defineUsefulConfig } from 'unocss-preset-useful'
 import transormerAttributifyJsx from "@unocss/transformer-attributify-jsx"
+import { presetUno, presetAttributify } from 'unocss'
 
 export default defineUsefulConfig(
   {
   },
   {
+    presets: [
+      presetUno(),
+      presetAttributify()
+    ],
     transformers: [
       //@ts-ignore
       transormerAttributifyJsx()
