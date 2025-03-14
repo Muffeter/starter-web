d8680ab50e48b5e05717f25c37298df2e38c9989
sonmu <sonmu.def@gmail.com>
2025-03-14 17:26:49 +0800

    fix unocss path import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
 
+import { defineConfig as UnoConfig, presetUno, presetAttributify, transformerAttributifyJsx } from 'unocss'
+const unoConfig = UnoConfig(
+  {
+    presets: [
+      presetUno(),
+      presetAttributify()
+    ],
+    transformers: [
+      // @ts-ignore
+      transformerAttributifyJsx()
+    ]
+  }
+)
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [UnoCSS(), react()],
+  plugins: [UnoCSS(unoConfig), react()],
 })
